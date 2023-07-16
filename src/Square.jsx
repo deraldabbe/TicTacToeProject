c5827a29d4f400860e8f2f96d3838daaed8ac5ff
Derald Abbe <deraldabbe1@gmail.com>
2023-07-15 21:34:23 -0600

    project commit

--- src/Square.jsx
+++ src/Square.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 
+
 const Square = (props) => {
-  return <div className="square">{}</div>;
-  console.log(props.square, porps.player)
+  const handleClick = () => {
+    if (!props.squareValue) {
+      if (props.player) {
+        props.squares.splice(props.index, 1, "X");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      } else {
+        props.squares.splice(props.index, 1, "O");
+        props.setSquares(props.squares);
+        props.setPlayer(!props.player);
+      }
+    }
+  };
+  return (
+    <div className="square" onClick={handleClick}>
+      {props.squareValue === "O" ? <img src="https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/devmountain/original.png?1568083750" /> : props.squareValue}
+    </div>
+  );
 };
 
 export default Square;
